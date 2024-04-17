a344dee8c163261038c0035a374d5b9def6bbdfa
Miura <miura.mc23@gmail.com>
2024-04-17 15:30:22 +0900

    2章p88

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
 //    long l = 5 + 2L;
 //    System.out.println(d);
 //    System.out.println(l);
-    String msg = "私の年齢は" + 23;
-    System.out.println(msg);
+//    String msg = "私の年齢は" + 23;
+//    System.out.println(msg);
+
+    int a = 5;
+    int b = 3;
+    int m = Math.max(a, b);
+    System.out.println("比較実験：" + a + "と" + b + "とで大きい方は・・・" + m);
   }
 }
