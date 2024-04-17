b2116c310bf531164984e1a12872dfe9ecbe8c65
Miura <miura.mc23@gmail.com>
2024-04-17 16:02:20 +0900

    2章修了

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,26 @@ public class Main {
 //    int n = Integer.parseInt(age);
 //    System.out.println(
 //            "あなたは来年、" + (n + 1) +"際になりますね。"
-    int r = new java.util.Random().nextInt(90
-    );
-    System.out.println(r + "歳ですね");
+//    int r = new java.util.Random().nextInt(90
+//    );
+////    System.out.println(r + "歳ですね");
+//    System.out.println("名前入力");
+//    String name = new java.util.Scanner(System.in).nextLine();
+//    System.out.println("年齢入力");
+//    int age = new java.util.Scanner(System.in).nextInt();
+//    System.out.println("ようこそ" + age + "歳の" + name + "さん。");
+//    int x = 5;
+//    int y = 10;
+//    String ans = "x+yは" + (x + y);
+//    System.out.println(ans);
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("あなたの名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください");
+    int ageString = new java.util.Scanner(System.in).nextInt();
+    int fortune = new java.util.Random().nextInt(3);
+    fortune ++;
+    System.out.println(ageString + "歳の" + name + "さん、あなたの運気番号は"+fortune+"です。" );
+    System.out.println("1：大吉　2：中吉　3：吉　4：凶");
   }
 }
